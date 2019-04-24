6de143a91fcb9ccd4c00611493e7dd036966e151
qq20004604 <20004604@qq.com>
2019-04-24 22:52:59 +0800

    基本上可以跑代码了

---
+++ mysql_lingling/__init__.py
@@ -0,0 +1,112 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from mysql import connector
+
+_user = ''
+_password = ''
+_database = ''
+_host = '127.0.0.1'
+_port = 3306
+_unix_socket = None
+
+
+class MySQLTool(object):
+    def __init__(self, **args):
+        self.c = None
+        self.cursor = None
+        # 参数不足2个则直接扔掉，因为至少需要 root 和 password
+        if not ('user' in args and 'password' in args):
+            pass
+        else:
+            # 超过3个，取传的参数的值
+            self.args = args
+
+    # 连接到数据库，参数要么传值，要么使用默认值
+    # 这里的默认值是从 mysql/connector/abstracts.py 复制来的
+    def connect(self,
+                user=_user,
+                password=_password,
+                database=_database,
+                host=_host,
+                port=_port,
+                unix_socket=_unix_socket):
+        self.c = connector.connect(user=user,
+                                   password=password,
+                                   database=database,
+                                   host=host,
+                                   port=port,
+                                   unix_socket=unix_socket)
+        self.cursor = self.c.cursor()
+
+    # with 的时候执行，返回值是 with...as e 中的e的值
+    def __enter__(self):
+        self.connect(**self.args)
+        return self
+
+    # with 内部代码块执行完毕后执行
+    def __exit__(self, exc_type, exc_value, traceback):
+        if exc_type:
+            print('Error')
+        else:
+            self.close()
+
+    # 执行 SQL 语句，并返回最后一次查询的查询结果
+    def run_sql(self, sql_list):
+        # 依次执行 sql 语句
+        for sql in sql_list:
+            if len(sql) == 1:
+                self.cursor.execute(sql[0])
+            else:
+                self.cursor.execute(*sql)
+        return self.cursor.fetchall()
+
+    # 返回 cursor
+    def get_cursor(self):
+        return self.cursor
+
+    # 当上一次操作是插入时，获取插入的行数
+    # 如果是 -1，表示上一次操作不是插入
+    def get_insert_rowcount(self):
+        return self.cursor.rowcount
+
+    # 手动提交事务，部分场景下可能有用
+    def commit(self):
+        self.c.commit()
+
+    # 关闭连接
+    def close(self):
+        self.cursor.close()
+        self.c.commit()
+        self.c.close()
+
+
+# 测试代码和示例代码
+if __name__ == '__main__':
+    # 测试数据
+    user = 'docker'
+    pw = '1654879wddgfg'
+    # ip = '172.17.0.2'
+    database = 'docker_test_database'
+
+    # ---- 测试代码2 ----
+    with MySQLTool(user=user, password=pw, database=database) as m2:
+        result2 = m2.run_sql([
+            ['insert person(name, age) values ("李四", 20), ("王五", 30)'],
+            ['select * from person']
+        ])
+        print(result2)
+
+    # ---- 测试代码1 ----
+    m = MySQLTool()
+    # 查看mysql容器内 ip，参考这个链接：https://blog.csdn.net/CSDN_duomaomao/article/details/75638544
+    m.connect(user=user,
+              password=pw,
+              # host=ip,
+              database=database)
+    result = m.run_sql([
+        ['insert person(name,age) values ("李四", 20)'],
+        ['select * from person']
+    ])
+    print(result)
+    m.close()
--- test.py
+++ test.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def dtest(a, b, *c):
-    print(a, b, *c)
+
+def test_a(a=1, b=2, c=3):
+    print(a, b, c)
 
 
-d = [
-    (1, 2, 3),
-    (1, 2)
-]
-for i in d:
-    dtest(*i)
+def test_b(**args):
+    a = args
+    print(args)
+    test_a(**a)
+
+
+test_b(a=4, b=5)
