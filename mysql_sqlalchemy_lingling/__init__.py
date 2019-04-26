0a7dd4a5e86a5201efbaa6bb890e8ded319d7f20
qq20004604 <20004604@qq.com>
2019-04-26 15:47:20 +0800

    增删查改都行了

--- mysql_sqlalchemy_lingling/__init__.py
+++ mysql_sqlalchemy_lingling/__init__.py
@@ -4,7 +4,7 @@
 # 一些复杂用法参照 https://zhuanlan.zhihu.com/p/27400862
 
 # 导入:
-from sqlalchemy import Column, INT, String, create_engine
+from sqlalchemy import Column, INT, String, create_engine, MetaData
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -36,7 +36,7 @@ class SQLAlchemyTool(object):
 	def fast_create_engine(self,
 						   sql='mysql',
 						   drive='mysqlconnector',
-						   user='test',
+						   user='root',
 						   password='',
 						   host='127.0.0.1',
 						   port=3306,
@@ -52,14 +52,7 @@ class SQLAlchemyTool(object):
 	def close(self):
 		self.session.close()
 
-	# 拉取数据（全部）
-	def query_all(self, class_prototype, *filters):
-		if len(filters) > 0:
-			items = self.session.query(class_prototype).filter(*filters).all()
-		else:
-			items = self.session.query(class_prototype).all()
-		return items
-
+	# --------------------------------------
 	# 拉取数据（单个）
 	# 注意，如果返回多个，则会报错
 	def query_one(self, class_prototype, *filters):
@@ -69,14 +62,23 @@ class SQLAlchemyTool(object):
 			one = self.session.query(class_prototype).one()
 		return one
 
-	# 拉取数据（指定id），这个id是根据 primary_key = True 而定的，通常只能返回一个
-	def query_bykey(self, class_prototype, n, *filters):
+	# 拉取数据（全部）
+	def query_all(self, class_prototype, *filters):
 		if len(filters) > 0:
-			one = self.session.query(class_prototype).filter(*filters).get(n)
+			items = self.session.query(class_prototype).filter(*filters).all()
 		else:
-			one = self.session.query(class_prototype).get(n)
+			items = self.session.query(class_prototype).all()
+		return items
+
+	# 拉取数据（指定id），这个id是根据 primary_key = True 而定的，通常只能返回一个
+	def query_by_primary_key(self, class_prototype, key, *filters):
+		if len(filters) > 0:
+			one = self.session.query(class_prototype).filter(*filters).get(key)
+		else:
+			one = self.session.query(class_prototype).get(key)
 		return one
 
+	# --------------------------------------
 	# 插入一个
 	def insert_one(self, class_prototype, **args):
 		new_item = class_prototype(**args)
@@ -89,13 +91,33 @@ class SQLAlchemyTool(object):
 			self.session.add(class_prototype(**arg))
 		self.session.commit()
 
+	# --------------------------------------
+	# 符合过滤器的都删除
+	# 支持多个条件
+	def delete_byfilters(self, class_prototype, *filters):
+		self.session.query(class_prototype).filter(*filters).delete()
+		self.session.commit()
+
+	# --------------------------------------
+	# 改，根据id筛选出符合条件的，然后修改为指定值（可以只修改一个值）
+	def update_by_primary_key(self, class_prototype, result_dict, *filters):
+		self.session.query(class_prototype).filter(*filters).update(result_dict)
+
+# --------------------------------------
+# 创建表格测试
+# def create_table(self, class_prototype):
+# 	self.session.
+
+
+# 指定表里内容全删除
+
 
 if __name__ == '__main__':
 	# 创建对象的基类:（这里还是要自己手动声明的，因为要继承）
 	Base = declarative_base()
 
 
-	# 1# 测试代码，插入一个
+	# 1# 测试代码，插入一个 id=1
 	# 定义对象:
 	class TableTest(Base):
 		# 表的名字:
@@ -106,36 +128,97 @@ if __name__ == '__main__':
 		# primary_key=True 表示是主键
 		id = Column(INT(), autoincrement=True, primary_key=True)
 		name = Column(String(255))
-		age = Column(String(20))
+		age = Column(INT())
 
 
-	# 插入一个
-	with SQLAlchemyTool(user='docker', password='1654879wddgfg', database='docker_test_database') as t:
-		print("1#插入 name='测试1#' ,age=15")
-		t.insert_one(TableTest, name='测试1#', age=15)
-
-	# 2# 测试代码，插入多个
-	with SQLAlchemyTool(user='docker', password='1654879wddgfg', database='docker_test_database') as t:
-		print("2#插入 name='测试2#-1' ,age=14和name='测试2#-2' ,age=13")
-		t.insert_some(TableTest, (
-			{'name': '测试2#-1', 'age': 14},
-			{'name': '测试2#-2', 'age': 13}
-		))
-
-	# 3# 测试代码，测试 get(n)
-	# 指定固定id查询
-	with SQLAlchemyTool(user='docker', password='1654879wddgfg', database='docker_test_database') as t:
-		r = t.query_bykey(TableTest, 2)
-		print("3# 测试代码")
-		print(r.id, r.name, r.age)
-
-	# 4# 测试代码，请求全部 + 使用filter
-	with SQLAlchemyTool(user='docker', password='1654879wddgfg', database='docker_test_database') as t:
-		# 可以同时单个filter，或者多个filter一起用，也可以不用filter
-		print("4# 测试代码")
-		r = t.query_all(TableTest, TableTest.id < 8, TableTest.age < 30)
+	def show_all(t):
+		r = t.query_all(TableTest)
+		print('_______显示当前全部_______')
+		print("id,  name,   age")
 		for i in r:
 			print(i.id, i.name, i.age)
+		print('_______显示结束_______')
+		print('')
+
+
+	# 初始化，删除 > 0
+	with SQLAlchemyTool(user='docker', password='1654879wddgfg', database='docker_test_database') as t:
+		t.delete_byfilters(TableTest, TableTest.id > 0)
+		print('初始化结束')
+		show_all(t)
+
+	# 1# 插入一个
+	with SQLAlchemyTool(user='docker', password='1654879wddgfg', database='docker_test_database') as t:
+		print("==== 1# 测试代码：\n插入 id=1, name='测试1#' ,age=15")
+		t.insert_one(TableTest, id=1, name='测试1#', age=15)
+		show_all(t)
+
+	# 2# 测试代码，测试 get(n)
+	# 指定固定id查询
+	with SQLAlchemyTool(user='docker', password='1654879wddgfg', database='docker_test_database') as t:
+		r = t.query_by_primary_key(TableTest, 1)
+		print("==== 2# 测试代码：指定 primary_key 查询")
+		print("id,  name,   age")
+		print(r.id, r.name, r.age)
+		print('')
+
+	# 3# 测试代码，删除 id=1
+	with SQLAlchemyTool(user='docker', password='1654879wddgfg', database='docker_test_database') as t:
+		# 可以同时单个filter，或者多个filter一起用，但【必须用filter】
+		print("==== 3# 测试代码：\n删除 id=1，然后查询")
+		t.delete_byfilters(TableTest, TableTest.id == 1)
+		show_all(t)
+
+	# 4# 测试代码，插入多个
+	with SQLAlchemyTool(user='docker', password='1654879wddgfg', database='docker_test_database') as t:
+		print(
+			"==== 4# 测试代码：\n插入(id=1, name='测试2#-1' ,age=14),"
+			" (id=2, name='测试2#-2' ,age=20),"
+			" (id=3, name='测试2#-3' ,age=13)"
+		)
+		t.insert_some(TableTest, (
+			{'id': 1, 'name': '测试2#-1', 'age': 14},
+			{'id': 2, 'name': '测试2#-2', 'age': 20},
+			{'id': 3, 'name': '测试2#-3', 'age': 13}
+		))
+		show_all(t)
+
+	# 5# 测试代码，请求全部 + 使用filter
+	with SQLAlchemyTool(user='docker', password='1654879wddgfg', database='docker_test_database') as t:
+		# 可以同时单个filter，或者多个filter一起用，也可以不用filter
+		print("==== 5# 测试代码：\n 多个filter联动，查询 id < 8, age < 20")
+		r = t.query_all(TableTest, TableTest.id < 8, TableTest.age < 20)
+		print("id,  name,   age")
+		for i in r:
+			print(i.id, i.name, i.age)
+		print('')
+
+	# 6# 测试代码，update
+	with SQLAlchemyTool(user='docker', password='1654879wddgfg', database='docker_test_database') as t:
+		print(
+			"==== 6# 测试代码：\n将 (id=3, name='测试2#-3' ,age=13)，更新为 age=23\n"
+			"并将id < 3的age值更新为 10"
+		)
+		t.update_by_primary_key(TableTest, {"age": 23}, TableTest.id == 3)
+		t.update_by_primary_key(TableTest, {"age": 10}, TableTest.id < 3)
+		show_all(t)
+
+
+	class TableCreateTest:
+		# 表的名字:
+		__tablename__ = 'create_test'
+
+		# 表的结构:
+		# autoincrement=True 表示主键自增
+		# primary_key=True 表示是主键
+		id = Column(INT(), autoincrement=True, primary_key=True)
+		name = Column(String(255))
+
+
+	# 7# 测试代码，create table
+	# with SQLAlchemyTool(user='docker', password='1654879wddgfg', database='docker_test_database') as t:
+	#
+	# 	show_all(t)
 
 # 创建Query查询，filter是where条件，最后调用one()返回唯一行，如果调用all()则返回所有行:
 # user = session.query(User).all()[0]
