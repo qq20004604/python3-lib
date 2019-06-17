f98f46f5a3c9ca6159a2ec7ba3bd266c3061adda
qq20004604 <20004604@qq.com>
2019-06-18 00:27:52 +0800

    更新mysql模块的log系统

--- mysql_lingling/__init__.py
+++ mysql_lingling/__init__.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import sys
 from mysql import connector
 from printcolor_lingling import print_testresult
+import time
 
 _user = ''
 _password = ''
@@ -12,9 +14,22 @@ _port = 3306
 _unix_socket = None
 
 
-def log(msg):
-    with open('./log.log', 'wa')as f:
-        f.write(msg)
+def errlog(msg):
+    with open('./log/mysql-err.log', 'a')as f:
+        f.write('%s||%s：%s\n' % (
+            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+            sys._getframe(1).f_code.co_name,  # 执行errlog这个函数的函数名字，即上一级函数
+            msg
+        ))
+
+
+def sqllog(sql, args=[]):
+    with open('./log/mysql.log', 'a')as f:
+        f.write('%s||%s：%s\n' % (
+            time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()),
+            sys._getframe(1).f_code.co_name,  # 执行errlog这个函数的函数名字，即上一级函数
+            sql % tuple(args)
+        ))
 
 
 class MySQLTool(object):
@@ -58,51 +73,116 @@ class MySQLTool(object):
             self.close()
 
     # 执行 SQL 语句，并返回最后一次查询的查询结果
+    # 如果错误，返回False，其他时候返回结果
+    # 适用于select时使用。建议insert和update使用专有函数
     def run_sql(self, sql_list):
-        # 依次执行 sql 语句
-        for sql in sql_list:
-            print(sql)
-            if len(sql) == 1:
-                self.cursor.execute(sql[0])
-            else:
-                self.cursor.execute(*sql)
         error = False
         try:
+            # 依次执行 sql 语句
+            for sql in sql_list:
+                print('sql: %s', sql)
+                if len(sql) == 1:
+                    self.cursor.execute(sql[0])
+                    sqllog(sql[0])
+                else:
+                    self.cursor.execute(*sql)
+                    sqllog(sql[0], sql[1])
             # 这里如果报错，说明操作是比如 create table 之类的操作，返回 None
             result = self.cursor.fetchall()
         except BaseException as e:
+            print(e)
             error = True
-            log(str(e))
+            errlog(str(e))
         finally:
+            # 如果错误，返回False，其他时候返回结果
             if error:
                 return False
             else:
                 return result
 
-    # 同时插入多行，如果错误，会返回False
-    def insert_more_rows(self, sql, args):
+    # 更新专用函数。
+    # 错误返回False，未更新（比如说不需要更新）返回0，成功更新返回更新的行数，例如影响了2行数据就是2
+    def update_row(self, sql, args):
         error = False
-        msg = ''
+        change_rows = None
         try:
-            self.cursor.executemany(sql, args)
+            self.cursor.execute(sql, args)
+            sqllog(sql, args)
+            # 判断一下影响了多少条数据，如果为0条，说明啥事都没干
+            change_rows = self.get_cu_rowcount()
+        except BaseException as e:
+            print(e)
+            error = True
+            errlog(str(e))
+        finally:
+            # 如果错误，返回False，其他时候返回结果
+            if error:
+                return False
+            else:
+                return change_rows
+
+    # 插入一行。
+    # 插入失败（报错或无法插入）返回False，插入成功返回插入行的id
+    def insert_row(self, sql, args):
+        error = False
+        try:
+            # 正常情况下，返回值为None
+            self.cursor.execute(sql, args)
+            sqllog(sql, args)
+            # 判断一下影响了多少条数据，如果为0条，说明啥事都没干
+            change_rows = self.get_cu_rowcount()
+            if change_rows == 0:
+                error = True
         except BaseException as e:
             error = True
             msg = str(e)
-            log(msg)
+            errlog(msg)
         finally:
             if error:
-                return msg
+                return False
             else:
-                return True
+                return self.get_last_insert_id()
+
+    # 同时插入多行（也可以只插入一行）
+    # 如果插入错误/或未成功插入，会返回False
+    # 插入成功，则返回插入的条数
+    # 示例：m.insert_more_rows(
+    #             'insert person(name,age) values (%s, %s)',
+    #             [('六六六', 666)]
+    #         )
+    def insert_more_rows(self, sql, args):
+        error = False
+        change_rows = None
+        try:
+            # 正常情况下，返回值为None
+            self.cursor.executemany(sql, args)
+            sqllog(sql, args)
+            # 判断一下影响了多少条数据，如果为0条，说明啥事都没干
+            change_rows = self.get_cu_rowcount()
+            if change_rows == 0:
+                error = True
+        except BaseException as e:
+            error = True
+            msg = str(e)
+            errlog(msg)
+        finally:
+            if error:
+                return False
+            else:
+                return change_rows
 
     # 返回 cursor
     def get_cursor(self):
         return self.cursor
 
-    # 当上一次操作是插入时，获取插入的行数
+    # 当上一次操作是插入或更新时，获取插入/更新影响的行数。比如插入一条就是一行
     # 如果是 -1，表示上一次操作不是插入
-    def get_insert_rowcount(self):
+    def get_cu_rowcount(self):
         return self.cursor.rowcount
+
+    # 获取上一个插入行的id（需要要插入后执行才能正确返回id，否则返回None）
+    def get_last_insert_id(self):
+        return self.cursor.lastrowid
 
     # 手动提交事务，部分场景下可能有用
     def commit(self):
@@ -137,6 +217,7 @@ if __name__ == '__main__':
             # 打印结果
             print(result2)
 
+        print("——————————————————————")
         # ---- 测试代码1 ----
         m = MySQLTool()
         # 查看mysql容器内 ip，参考这个链接：https://blog.csdn.net/CSDN_duomaomao/article/details/75638544
@@ -144,13 +225,22 @@ if __name__ == '__main__':
                   password=pw,
                   # host=ip,
                   database=database)
-        result = m.run_sql([
-            ['insert person(name,age) values (%s, %s)', ['六六六', 666]],
-            ['select * from person']
-        ])
-        print(result)
+        result = m.insert_more_rows(
+            'insert person(name,age) values (%s, %s)',
+            [('六六六', 666)]
+        )
+        if result is False:
+            is_error = True
+
+        result2 = m.insert_row(
+            'insert person(name,age) values (%s, %s)',
+            ('六六六', 666)
+        )
+        if result is False:
+            is_error = True
+
         m.close()
     except BaseException as e:
+        print(e)
         is_error = True
-
-    print_testresult(~is_error, 'MySQLTool')
+    print_testresult(not is_error, 'MySQLTool')
